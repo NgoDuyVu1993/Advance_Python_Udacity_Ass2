6a6dc90f3939b7d29980fdc3d15633501de6db23
NgoDuyVu1993 <114485635+NgoDuyVu1993@users.noreply.github.com>
2023-11-05 16:35:12 +1000

    Delete QuoteEngine directory

--- QuoteEngine/__init__.py
+++
@@ -1,7 +0,0 @@
-from .IngestorInterface import IngestorInterface
-from .CSVIngestor import CSVIngestor
-from .DocxIngestor import DocxIngestor
-from .PDFIngestor import PDFIngestor
-from .TextIngestor import TextIngestor
-from .QuoteModel import QuoteModel
-from .Ingestor import Ingestor
